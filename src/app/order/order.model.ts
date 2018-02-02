c4630ce4ee1e72d685efc8b56b06455c1ee5d635
Artur Ramme <arthur.ramme@operacao.rcadigital.com.br>
2018-02-02 16:14:32 -0200

    finalização componentes do projeto

---
+++ src/app/order/order.model.ts
@@ -0,0 +1,13 @@
+class Order {
+  constructor(public address: string,
+    public number: number,
+    public optionalAddress: string,
+    public paymentOptional: string,
+    public orderItem: OrderItem[] = []) { }
+}
+
+class OrderItem {
+  constructor(public quantity: number, public menuId: string) { }
+}
+
+export { Order, OrderItem }
